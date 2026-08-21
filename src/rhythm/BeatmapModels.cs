bce2ae9371e106ca78c2a59afd8e20924e9499db
Pedro Nogueira <henriqs.yfg@gmail.com>
2026-08-21 06:04:47 -0300

    rhythm: link charts to reusable mapsets

--- src/rhythm/BeatmapModels.cs
+++ src/rhythm/BeatmapModels.cs
@@ -12,6 +12,9 @@ public sealed class BeatmapDocument
     [JsonPropertyName("id")]
     public string Id { get; set; } = string.Empty;
 
+    [JsonPropertyName("setId")]
+    public string SetId { get; set; } = string.Empty;
+
     [JsonPropertyName("title")]
     public string Title { get; set; } = string.Empty;
 
